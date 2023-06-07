be2e342ff78b63330bbaf0db77a9c9b147184e06
Nekstoreo <nestog456k@outlook.com>
2023-06-06 19:30:36 -0500

    2023-06-06

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,2 @@
-// main.js
 
-// Llamada a la API de Geonames para obtener la lista de ciudades
-fetch('http://api.geonames.org/searchJSON?country=ES&maxRows=10&username=demo')
-    .then(response => response.json())
-    .then(data => {
-        const selectElement = document.getElementById('ubicacion');
-        data.geonames.forEach(city => {
-            const option = document.createElement('option');
-            option.value = city.name;
-            option.textContent = city.name;
-            selectElement.appendChild(option);
-        });
-    })
-    .catch(error => console.log(error));
+  
